5b524d4bfa77ba966f97b98ce46144fcd1831fde
Tri2k6 <Cheatgaming647@gmail.com>
2025-12-10 13:31:47 +0700

    update THW6

--- THW6/main.cpp
+++ THW6/main.cpp
@@ -36,32 +36,42 @@ int main() {
             ArrInt.PushBack(val);
         }
 
+        ArrInt.print();
+
         cout << "Max Element: " << ArrInt.MaxElement() << endl;
+ 
+
+        int idx;
+        cout << "Nhap vi tri cua phan tu can lay (0 -> n - 1): "; cin >> idx;
+        cout << "Gia tri tai vi tri " << idx << ": " << ArrInt.GetAt(idx) << endl;
+
         ArrInt.Sort();
         cout << "Sorted Array<int>: \n";
         ArrInt.print();
 
-        int idx;
-        cout << "Nhap vi tri cua phan tu can lay (0 - n): "; cin >> idx;
-        cout << "Gia tri tai " << idx << ": " << ArrInt.GetAt(idx) << endl;
 
         cout << "----------------------------Array<Fraction>------------------------------\n";
         Dynamic_Array<Fraction> ArrFrac;
         Fraction Frac_tmp;
         cout << "Nhap so phan tu cua mang phan so: "; cin >> n;
         for (int i = 0;i < n;i++) {
-            cout << "Nhap phan tu thu " << i + 1 << ": ";
+            cout << "Nhap phan tu thu " << i + 1 << ": \n";
             cin >> Frac_tmp;
             ArrFrac.PushBack(Frac_tmp);
         }
 
+        ArrFrac.print();
+
         cout << "Max Element: " << ArrFrac.MaxElement() << endl;
+
+
+        cout << "Nhap vi tri cua phan tu can lay (0 -> n - 1): "; cin >> idx;
+        cout << "Gia tri tai vi tri " << idx << ": " << ArrFrac.GetAt(idx) << endl;
+
         ArrFrac.Sort();
         cout << "Sorted Array<Fraction>: \n";
         ArrFrac.print();
 
-        cout << "Nhap vi tri cua phan tu can lay (0 - n): "; cin >> idx;
-        cout << "Gia tri tai " << idx << ": " << ArrFrac.GetAt(idx) << endl;
 
     }
     catch(const std::exception& e)
